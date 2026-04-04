04238b23bcf2799daccb595489dac1dce3afc082
Student <Student@LabVM2047644.a1nj0vouhuruhgprpqardq2f0f.jnbx.internal.cloudapp.net>
2026-04-04 15:25:48 +0200

    Added a create hash message id method and one to create the message id

--- src/main/java/com/mycompany/chat_application/Message.java
+++ src/main/java/com/mycompany/chat_application/Message.java
@@ -10,7 +10,34 @@ package com.mycompany.chat_application;
  */
 public class Message {
     
+    void Display(){
+        
+        System.out.println("***Welcoome to QuickChat***\nOption1 : Send Messages"
+                                + " \nOption2: Show recently sent messages  "
+                                + "\nOption3: Quit");
+        
     
+}
+    
+    public String createMessageHash(long id, int numOfMessage, String message){
+        
+       String ID = Long.toString(id);
+       String firstTwoNUm = ID.substring(0, 2);
+       
+       int firstSpace = message.indexOf(" ");
+       String firstWord = message.substring(0, firstSpace).toUpperCase();
+       
+       int lastSpace = message.lastIndexOf(" ");
+       String lastWord = message.substring(lastSpace + 2).toUpperCase();
+       
+       return firstTwoNUm + ":" + numOfMessage + firstWord + lastWord;
+    }
+    
+    
+    public long createMessageId(){
+        long id = 1_000_000_000L + (long)(Math.random() * 9_000_000_000L);
+        return id;
+    }
     public boolean checkRecipientCellphoneNum(String cellPhoneNumber){
         
         return cellPhoneNumber.matches("^\\+27\\d{9}$");
